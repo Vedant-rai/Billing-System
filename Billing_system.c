ab9964352ba5bbc0a1b4ce760c1a7bf461ec7250
Vedant-rai <96318619+Vedant-rai@users.noreply.github.com>
2022-01-26 21:11:10 +0530

    Added comments

--- Billing_system.c
+++ Billing_system.c
@@ -3,14 +3,14 @@
 #include<string.h>
 #include<unistd.h>
 
-void generatebillheading(char name[50],char date[30],char time[30]) //two arguments for name and date
+void generatebillheading(char name[50],char date[30],char time[30]) //three arguments for name, date and time
 {
     printf("\n\n");
     // \t is used for spacing(4 spaces)
     printf("        .-----------------------.");
-    printf("\n\t|                       |\n\t|     ABC RESTAURANT    |");
+    printf("\n\t|                       |\n\t|     ABC RESTAURANT    |");    // name of the restaurant
     printf("\n\t|                       |");
-    printf("\n        '-----------------------'\n"); // name of the restaurant
+    printf("\n        '-----------------------'\n"); 
     
     printf("\n-----------------------------------------\n");
     printf("\nDate:%s",date);
@@ -44,7 +44,7 @@ void generatebillfooter(float total)
     printf("\n---------------------------------------\n");
 }
 
-// function for bill body taking input of item price and qty of material and displaying
+// function for bill body taking input of item, price and qty of material and displaying
 void generatebillbody(char item[30],int qty,float price)
 {
      printf("%s\t\t",item);
@@ -52,7 +52,8 @@ void generatebillbody(char item[30],int qty,float price)
      printf("%.2f\t\t",qty*price);
      printf("\n");
 }
- struct items
+//declaring stuctures
+ struct items       
  {
   char item[20];
   float price;
@@ -78,11 +79,11 @@ struct details
 
 int main()
 {
-    FILE *lg;
+    FILE *lg;       //declaring file pointer for login details
     int option;
     char name1[30],psword[30],cpsword[30];
     char Saveinfo = 'y';
-    struct details detail,det,detmatch;
+    struct details detail,det,detmatch;     //variable declaration of structure
     system("clear");
     printf("\n\t........Choose your desired option........\n\n");
     printf("\n\n1.Create Account\n");
@@ -90,15 +91,16 @@ int main()
     printf("\n\nYour Choice:\t\t");
     scanf("%d",&option);
 
-    switch(option)
+    // switch case for different choices
+    switch(option)      
     {   
         case 1:
 
         fgetc(stdin);
-        system("clear");
+        system("clear");        //to clear previous output from the screen
         printf("\n\nEnter your name:\t ");
-        fgets(detail.name,30,stdin);
-        detail.name[strlen(detail.name)-1]=0;
+        fgets(detail.name,30,stdin);        // to take string as an input from user
+        detail.name[strlen(detail.name)-1]=0;    //to assign 0 at the end of string
         printf("\n\nCreate your username:\t");
         fgets(detail.username,30,stdin);
         detail.username[strlen(detail.username)-1]=0;
@@ -106,21 +108,21 @@ int main()
         fgets(detail.password,30,stdin);
         detail.password[strlen(detail.password)-1]=0;
         printf("\n\nConfirm password:\t");
-        scanf("%s",cpsword);
-        if(strcmp(detail.password,cpsword)==0){
+        scanf("%s",cpsword);                // temporary character array to check password
+        if(strcmp(detail.password,cpsword)==0){     //strcmp returns 0 if strings match
         printf("\n\n\tDo You want to save the details?[y/n]\t");
         scanf("%s",&Saveinfo);
         if(Saveinfo=='y')
         {
-            lg=fopen("logindetails.dat","a+");
-            fwrite(&detail,sizeof(struct details),1,lg);
-            if(fwrite!=0)
+            lg=fopen("logindetails.dat","a+");          //open login file in append mode
+            fwrite(&detail,sizeof(struct details),1,lg);    //to write data into the file in form of structure
+            if(fwrite!=0)                   //if data is stored in file
             printf("\n\t\tSuccessfully Saved");
             
             else
             printf("\n Error Occurred");
-            fclose(lg);
-            goto label;
+            fclose(lg);      //close file
+            goto label;      //to jump to case 2
             
         }
         break;
@@ -129,24 +131,24 @@ int main()
 
         label:
         fgetc(stdin);
-        system("clear");
+        system("clear");        //to clear previous output
         printf("\n\n\n\t\tLogin\n\n");
         printf("\n\nEnter Your Username:\t");
-        fgets(name1,30,stdin);
-        name1[strlen(name1)-1]=0;
+        fgets(name1,30,stdin);          // to take string as an input from user
+        name1[strlen(name1)-1]=0;       //to assign 0 at the end of string
         printf("\n\nEnter Your Password:\t");
         fgets(psword,30,stdin);
         psword[strlen(psword)-1]=0;
 
-            lg =fopen("logindetails.dat","r+");
-            while(fread(&detmatch,sizeof(struct details),1,lg))
+            lg =fopen("logindetails.dat","r+");         //open login file in read mode
+            while(fread(&detmatch,sizeof(struct details),1,lg))     //to read data from file one by one
             {   
                 
                 if((strcmp(detmatch.username,name1)==0)&&(strcmp(detmatch.password,psword)==0))
                 {       system("clear");
                         printf("\n\n\t\tWELCOME %s\n",detmatch.name);
-                        sleep(2);
-                        FILE *fp, *fp1;
+                        sleep(2);           // to pause the screen for 2 seconds
+                        FILE *fp, *fp1;     //declaring file pointers for customer details
                         char SaveBill = 'y',usrfile[50],usrfile1[50];
                         struct orders ord;
                         struct orders order;
@@ -154,7 +156,7 @@ int main()
                         int size,opt,n,found,continuee=0;
                     
                         char name[50];
-                        while(continuee==0)
+                        while(continuee==0)     // to loop the main menu
                         {   system("clear");
                             float total=0;
                             int invoicefound=0;
@@ -173,14 +175,14 @@ int main()
                             fgetc(stdin); //to consume \n generated by fgets
                             switch(opt)
                         {
-                            case 1:
+                            case 1:     //for generating invoice
 
                             system("clear");
                             printf("\nPlease Enter The Name of The Customer :  ");
                             fgets(ord.customer,50,stdin); // taking name of customer
                             ord.customer[strlen(ord.customer)-1] = 0; //to assign 0 at the end of string
                             strcpy(ord.date,__DATE__);  //DATE is predefined
-                            strcpy(ord.time,__TIME__);
+                            strcpy(ord.time,__TIME__);  //TIME is predefined
                             printf("\nPlease Enter The Number Of The Items:\t"); // number of items customer bought
                             scanf("%d",&n);
                             ord.numofitems=n; // assigning n to numofitems
@@ -200,7 +202,7 @@ int main()
                             }
                             generatebillheading(ord.customer,ord.date,ord.time); // give name and predefined date as argument
                             for(int i=0;i<ord.numofitems;i++)
-                            {                                //
+                            {                                
                                  generatebillbody(ord.itm[i].item,ord.itm[i].qty,ord.itm[i].price);
                             }
                     
@@ -209,7 +211,7 @@ int main()
                             scanf("%s",&SaveBill);
                     
                             if(SaveBill=='y')  // for saving the bill file management
-                            {   sprintf(usrfile,"%s.dat",name1);
+                            {   sprintf(usrfile,"%s.dat",name1);        // to store file name as user name in a string
                                 fp=fopen(usrfile,"a+"); // ab for creating the file
                                 fwrite(&ord,sizeof(struct orders),1,fp);
                                 if(fwrite!=0){
@@ -221,13 +223,13 @@ int main()
                             }
                             break;
                     
-                            case 2:
+                            case 2:     //to show all invoices
 
                             system("clear");
                             sprintf(usrfile,"%s.dat",name1);
-                            if(fp =fopen(usrfile,"r+"))
+                            if(fp =fopen(usrfile,"r+"))      // if file exists
                             {   printf("\n\t*Your Previous Invoices*");
-                                while (fread(&order,sizeof(struct orders),1,fp))
+                                while (fread(&order,sizeof(struct orders),1,fp))    // to read data from file one by one
                                 {   
                                     float tot=0;
                                     generatebillheading(order.customer,order.date,order.time);
@@ -240,10 +242,10 @@ int main()
                                     generatebillfooter(tot);
                                     printf("\n\n\n");
                                 }
-                                if (fp !=NULL) 
+                                if (fp !=NULL)      //if file exists
                                 {
-                                    fseek (fp, 0, SEEK_END);
-                                    size = ftell(fp);
+                                    fseek (fp, 0, SEEK_END);   // to move file pointer to the end of user file
+                                    size = ftell(fp);       //to store the current file position wrt starting
                     
                                     if (0 == size) 
                                     {
@@ -259,22 +261,22 @@ int main()
                             }
                             break;
                     
-                            case 3:
+                            case 3:     //to search invoice
 
                             printf("\nEnter The Name of The Customer:\t");
-                            fgets(name,50,stdin);
-                            name[strlen(name)-1]=0;
+                            fgets(name,50,stdin);       // taking name of customer
+                            name[strlen(name)-1]=0;     //to assign 0 at the end of string
                             system("clear");
                             sprintf(usrfile,"%s.dat",name1);
-                            if(fp =fopen(usrfile,"r+"))
+                            if(fp =fopen(usrfile,"r+"))     //if file exists
                             {   printf("\n\t*Invoice of %s*",name);
-                                while (fread(&ords,sizeof(struct orders),1,fp))
+                                while (fread(&ords,sizeof(struct orders),1,fp))     // to read data from file one by one
                                 {   
                                     float tot=0;
-                                    if(!strcmp(ords.customer,name))
+                                    if(!strcmp(ords.customer,name))     //strcmp returns 0 if strings match
                                     {
                                         generatebillheading(ords.customer,ords.date,ords.time);
-                                        for(int i=0;i<ords.numofitems;i++)
+                                        for(int i=0;i<ords.numofitems;i++)  //for different items
                                         {
                                             generatebillbody(ords.itm[i].item,ords.itm[i].qty,ords.itm[i].price);
                                             tot+=ords.itm[i].qty*ords.itm[i].price;
@@ -284,11 +286,11 @@ int main()
                                         invoicefound =1;
                                     }
                                 }
-                                if(!invoicefound)
+                                if(!invoicefound)   //if invoicefound=0
                                 {    system("clear");
                                     printf("\nSorry The Invoice For %s does not exist",name);
                                 }
-                                fclose(fp);
+                                fclose(fp); //close file
                             }
                             else
                             {
@@ -296,42 +298,42 @@ int main()
                             }
                             break;
                             
-                            case 4:
+                            case 4:     //for deleting invoice
 
                             printf("\nEnter name of invoice to delete: ");
                             fgets(name,50,stdin);
                             name[strlen(name)-1]=0;
                             system("clear");
                             sprintf(usrfile,"%s.dat",name1);
-                            if(fp = fopen(usrfile,"r")){
+                            if(fp = fopen(usrfile,"r")){       //if file exists
                             sprintf(usrfile1,"%stemp.dat",name1);
-                            fp1 = fopen(usrfile1,"w");     //temporary file to store data
+                            fp1 = fopen(usrfile1,"w");     //open temporary file in write mode to store data
                             found=0;
-                            while(fread(&ord,sizeof(struct orders),1,fp))
+                            while(fread(&ord,sizeof(struct orders),1,fp))    // to read data from file one by one
                             {
-                                if(strcmp(ord.customer,name)==0)    //strcmp returns 0    
+                                if(strcmp(ord.customer,name)==0)    // strcmp returns 0 if strings match    
                                 {   
                                     found=1;
                                 }
                                 else
                                 {
-                                    fwrite(&ord,sizeof(struct orders),1,fp1);
+                                    fwrite(&ord,sizeof(struct orders),1,fp1);   // write data in temp file
                                 }
                             }
-                            fclose(fp);
+                            fclose(fp);         //close files
                             fclose(fp1);
                             
                             
                             if(found==1)
                             {   
-                                fp1 = fopen(usrfile1,"r");
-                                fp = fopen(usrfile,"w");
+                                fp1 = fopen(usrfile1,"r");  //open temporary file in read mode
+                                fp = fopen(usrfile,"w");    //open user file in write mode
                                 
-                                while(fread(&ord,sizeof(struct orders),1,fp1)){
+                                while(fread(&ord,sizeof(struct orders),1,fp1)){     // to write data from temp file to user file
                                     fwrite(&ord,sizeof(struct orders),1,fp);
                                 }
                                 printf("\n\n\tInvoice Deleted Successfully\n");
-                                fclose(fp);
+                                fclose(fp);         //close files
                                 fclose(fp1);
                                 
                             }
@@ -339,16 +341,16 @@ int main()
                             {
                                 printf("\nSorry The Invoice For %s Does Not Exist\n",name);
                             }
-                            remove(usrfile1);
+                            remove(usrfile1);       // remove temporary file
                             }
                             else{
                                 printf("\nFile Does Not Exist\n");
                             }
                             break;
                     
-                            case 5:
+                            case 5:     //for ending the programme
                             printf("\n\n\t    Thank You For Using Our Service\n\n\n\n");
-                            exit(0);        //for ending the programme
+                            exit(0);        
                             break;
                     
                             default:
